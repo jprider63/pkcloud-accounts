26b8c4d4db3cb5051f1d3b0b1fbd1048c14f209e
hschallh <hschallh@gmail.com>
2017-09-08 13:49:24 -0400

    Modularized widgets in Home

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,20 +10,40 @@ import Import
 
 getHomeR :: Handler Html
 getHomeR = do
-    maybeUserId <- maybeAuthId
+    _ <- requireAuth
     defaultLayout $ do
         addStylesheet $ StaticR css_bootstrap_css
+        addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
         setTitle "PKCloud Accounts"
         [whamlet|
-            <nav .navbar .navbar-default>
-                <div .container>
-                    <div .navbar-header>
-                        <a .navbar-brand href=@{HomeR}>Home
-                    <ul .nav .navbar-nav .navbar-right>
-                        <li>
-                            $maybe _ <- maybeUserId
-                                <a href=@{AuthR LogoutR}>Logout
-                            $nothing
-                                <a href=@{AuthR LoginR}>Login
+            ^{alert}
+            ^{navbar}
+            <div .page-header>
+                <h1>Welcome to PKCloud Accounts
         |]
 
+alert :: Widget
+alert = do
+    mmsg <- getMessage
+    [whamlet|
+    $maybe msg <- mmsg
+        <div .alert .alert-success>#{msg}
+    |]
+    toWidget [julius|window.setTimeout(function() {
+        $(".alert-success").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove();
+        });
+    }, 2000);
+|]
+
+navbar :: Widget
+navbar = do
+    [whamlet|
+    <nav .navbar.navbar-default>
+        <div .container>
+            <div .navbar-header>
+                <a .navbar-brand href=@{HomeR}>Home
+            <ul .nav.navbar-nav.navbar-right>
+                <li>
+                    <a href=@{AuthR LogoutR}>Logout
+|]
